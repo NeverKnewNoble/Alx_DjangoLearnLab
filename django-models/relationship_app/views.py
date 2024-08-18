8fabed1828ed9190518ae8f93d03254826bd9c01
Sasuke Uchiha <apexpredator1607@outlook.com>
2024-08-18 22:17:34 +0000

    Configured admin view to be accessible only by users with Admin role

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -68,3 +68,12 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return HttpResponse("Welcome Member!")
+from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome Admin!")
