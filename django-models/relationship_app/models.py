5bb961d382d7f2093919b9050a22c5dda2e179ae
Sasuke Uchiha <apexpredator1607@outlook.com>
2024-08-18 22:09:07 +0000

    Configured admin view to be accessible only by users with Admin role

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -31,6 +31,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -38,13 +42,7 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
