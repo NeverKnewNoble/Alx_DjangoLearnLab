20785a96cdbcb3092ee59012153bb0b6a7894ba0
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-02 09:29:34 +0000

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 from rest_framework.authtoken.views import obtain_auth_token
