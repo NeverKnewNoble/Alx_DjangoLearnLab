21b68e96ece7fa9ce22396c621342cd2f710043c
Kelly07Norman <82865397+Kelly07Norman@users.noreply.github.com>
2024-09-02 09:27:59 +0000

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
