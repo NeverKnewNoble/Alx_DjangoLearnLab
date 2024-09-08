45c6b4615900380bb2557044c6fddf2d5307caaa
Kelly07Norman <kellynorman727@gmail.com>
2024-09-08 14:53:00 +0000

    Initial commit for advanced_api_project with custom and generic views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,7 +7,7 @@ from .views import (
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),                # List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),   # Detail of a single book
-    path('books/create/', BookCreateView.as_view(), name='book-create'),     # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/create/', BookCreateView.as_view(), name='book/create'),     # Create a new book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book/update'),  # Update a book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book/delete'),  # Delete a book
 ]
