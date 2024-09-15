876e5351d84262bf00b00d783459e7e9f2adb17a
Kelly07Norman <kellynorman727@gmail.com>
2024-09-15 17:02:09 +0000

    Update post_detail.html to include tagging functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,15 +1,21 @@
+# blog/views.py
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from django.db.models import Q
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
-class PostListView(ListView):
+# Post views
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
@@ -19,92 +25,56 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
-
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post_list')
+    template_name = 'blog/post_confirm_delete.html'
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
-
+# Comment views
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        form.instance.author = self.request.user
         form.instance.post_id = self.kwargs['post_id']
         return super().form_valid(form)
 
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-    def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
     model = Comment
+    success_url = reverse_lazy('post_list')
     template_name = 'blog/comment_confirm_delete.html'
 
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
+# Tag views
+class PostByTagListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
 
     def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)
+        tag_slug = self.kwargs.get('tag_slug')
+        return Post.objects.filter(tags__slug=tag_slug)
 
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
+# Search view
+class SearchResultsView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/search_results.html'
+    context_object_name = 'posts'
 
-
-from django import forms
-from .models import Post, Tag
-
-class PostForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(
-        queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
-    )
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
-
-from django.db.models import Q
-from django.shortcuts import render
-from .models import Post
-
-def search_posts(request):
-    query = request.GET.get('q')
-    if query:
-        posts = Post.objects.filter(
-            Q(title__icontains=query) | 
-            Q(content__icontains=query) | 
-            Q(tags__name__icontains=query)
-        ).distinct()
-    else:
-        posts = Post.objects.all()
-    return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)
+            ).distinct()
+        return Post.objects.none()
