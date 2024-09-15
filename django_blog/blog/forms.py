2549b8c5aa24f5b45aa92bd4fd26c28914ce28f7
Kelly07Norman <kellynorman727@gmail.com>
2024-09-15 16:50:21 +0000

    Update post_detail.html to include tagging functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,13 @@
 from django import forms
-from .models import Comment
+from taggit.forms import TagField  # Import TagField for handling tags
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    tags = TagField(required=False)  # Use TagField for tag input
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include 'tags' in fields
 
 class CommentForm(forms.ModelForm):
     class Meta:
