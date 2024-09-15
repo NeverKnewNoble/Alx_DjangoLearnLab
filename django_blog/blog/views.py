b8b88027fb5ce1be111e0994c9d1ace85be9bf74
Kelly07Norman <kellynorman727@gmail.com>
2024-09-15 16:25:28 +0000

    Added comment functionality to blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
@@ -52,17 +52,25 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
 
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
     def get_queryset(self):
         return Comment.objects.filter(author=self.request.user)
 
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
     def get_queryset(self):
         return Comment.objects.filter(author=self.request.user)
