91d4a6641d174ac5d0a8d016430b9c55fbfd4de0
Kelly07Norman <kellynorman727@gmail.com>
2024-09-15 16:27:09 +0000

    Added comment functionality to blog posts.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,15 +1,22 @@
-# blog/urls.py
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import add_comment, edit_comment, delete_comment
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:post_id>/comments/new/', add_comment, name='add_comment'),
-    path('comment/<int:pk>/edit/', edit_comment, name='edit_comment'),
-    path('comment/<int:pk>/delete/', delete_comment, name='delete_comment'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
