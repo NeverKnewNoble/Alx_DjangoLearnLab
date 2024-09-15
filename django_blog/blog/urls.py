d4fc12101f403f6161a35547e1dddf43f9badc1d
Kelly07Norman <kellynorman727@gmail.com>
2024-09-15 15:57:34 +0000

    Implement CRUD operations for blog posts with permissions

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('', PostListView.as_view(), name='post-list'),  # View all posts
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -51,3 +51,55 @@ def profile(request):
     else:
         form = UserChangeForm(instance=request.user)
     return render(request, 'registration/profile.html', {'form': form})
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post  # Assuming your Post model is defined in models.py
+from .forms import PostForm  # Assuming you created a PostForm in forms.py
+
+# ListView: Display all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Adjust path if necessary
+    context_object_name = 'posts'  # Use 'posts' in the template
+
+# DetailView: Show a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  # Adjust path if necessary
+
+# CreateView: Allow authenticated users to create new posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Automatically set the author
+        return super().form_valid(form)
+
+# UpdateView: Allow post authors to edit their posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only author can update
+
+# DeleteView: Allow post authors to delete their posts
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')  # Redirect to post list after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only author can delete
